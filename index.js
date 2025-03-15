788aac9d434062fb9f655d2f371e9efefd1cf86d
Akatsuki _hack <77784562+chuong9276@users.noreply.github.com>
2025-03-15 20:25:41 +0700

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Bot của Akatsuki Chương", ""];
+const statusMessages = ["Bot của Akatsuki Chương", "White Wolves { Bạch Lang } MOD BOT"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
